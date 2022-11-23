752f3990959250c2325cc85db8bd32d77cb4508a
Diegodelias <34484881+Diegodelias@users.noreply.github.com>
2022-11-22 21:45:08 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Canvas from "./componentes/Canvas";
+export default function App() {
+  return (
+    <div className="App">
+      
+        <Canvas />
+      
+    </div>
+  );
+}
